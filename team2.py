e582d9b7794b35fbb7c1bd7956d7edbdf78731cd
21SandersJ <21SandersJ@isd112.org>
2018-01-31 08:18:54 -0600

    Strategy
    Changed our teams strategy name and description for now.

--- team2.py
+++ team2.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Undefeated' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'strategy2'
+strategy_description = ''
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
